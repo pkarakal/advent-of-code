196acf4c51c49119be4d4e79dfaed049097837a0
Pavlos Karakalidis <pkarakal@pkarakal.com>
2023-12-02 17:15:25 +0200

    feat(bootstrap): add scaffold for advent of code

---
+++ common/src/lib.rs
@@ -0,0 +1,49 @@
+use std::{fs, io};
+use std::fmt::{Display, Formatter};
+use std::path::Path;
+use std::process::exit;
+
+#[derive(Debug, PartialEq)]
+pub enum Answer {
+    U32(u32),
+    USize(usize)
+}
+
+
+impl From<u32> for Answer {
+    fn from(n: u32) -> Self {
+        Self::U32(n as u32)
+    }
+}
+
+impl From<usize> for Answer {
+    fn from(n: usize) -> Self {
+        Self::USize(n as usize)
+    }
+}
+
+impl Display for Answer{
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Answer::USize(u) => write!(f, "{u}"),
+            Answer::U32(n) => write!(f, "{n}"),
+        }
+    }
+}
+
+
+pub trait Solution {
+    fn name(&self) -> String;
+    fn part_one(&self, input: &str) -> Answer;
+    fn part_two(&self, input: &str) -> Answer;
+}
+
+pub fn parse_file(file: &Path) -> io::Result<String>{
+    return fs::read_to_string(file);
+}
+
+pub fn load_file(year: u32, day: u32) -> io::Result<String> {
+    let file_name = format!("data/{}/{:02}.txt", year, day);
+    let file = Path::new(&file_name);
+    return parse_file(file);
+}
---
+++ src/main.rs
@@ -0,0 +1,53 @@
+use std::vec;
+use clap::Parser;
+use common::Solution;
+
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct AdventOfCode {
+    /// Year to use
+    #[arg(short, long, default_value_t = 2023)]
+    year: u32,
+
+    /// Day which problem to run
+    #[arg(short, long, default_value_t = 1)]
+    day: u32,
+
+    /// Part specifies which part of the given day to run
+    #[arg(short, long, default_value_t = 1)]
+    part: u32
+}
+
+
+
+fn main() {
+
+    let args = AdventOfCode::parse();
+
+    let solutions = get_solutions_for_year(args.year);
+
+    let solution = match solutions.get((args.day as usize) - 1) {
+        Some(s) => s,
+        None => {
+            println!("No solution for day {} in year {}", args.day, args.year);
+            return;
+        }
+    };
+
+    let data = common::load_file(args.year, args.day).unwrap();
+
+    let result = match args.part {
+        1 => solution.part_one(&data),
+        2 => solution.part_two(&data),
+        _ => unimplemented!()
+    };
+
+    println!("Result = {}", result);
+}
+
+fn get_solutions_for_year<'a>(year: u32) -> Vec<&'a dyn Solution> {
+    match year  {
+        _ => unimplemented!()
+    }
+}
